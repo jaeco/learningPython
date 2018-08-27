f9ff30dca7bf5b504dbd72f1cd76dd5b6a701842
James Cor <jaeco@users.noreply.github.com>
2018-08-27 14:35:40 -0700

    example 3 - math

---
+++ theHardWay/ex3.py
@@ -0,0 +1,29 @@
+print("I will now count my chickens:")
+
+#P E (M D) (A S)
+
+# 30 divide by 6 then add 25
+print("Hens", 25 + 30 / 6)
+
+# 25 times 3 = 75
+# 75 mod 4 = 18.75 ~> 18 || 18 * 4 = 72 - 75 = 3
+# 3 minus 100 = 97
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count my eggs")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5>= -2)
+print("Is it less or equal?", 5 <= -2)
