1be5e7562fde55ee4c9975901aa3d9d1aea22284
James Cor <jaeco@users.noreply.github.com>
2018-08-28 09:42:37 -0700

    example 3 - breakdown pt 2

--- theHardWay/ex3.py
+++ theHardWay/ex3.py
@@ -22,6 +22,7 @@ print("Now I will count my eggs")
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
 print("Is it true that 3 + 2 < 5 - 7?")
+# Do math then compare
 print(3 + 2 < 5 - 7)
 
 print("What is 3 + 2?", 3 + 2)
@@ -32,5 +33,5 @@ print("Oh, that's why it's False.")
 print("How about some more.")
 
 print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5>= -2)
+print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <= -2)
