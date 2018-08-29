14bc00792e80dd11c244280dcb4742d8230369b1
James Cor <jaeco@users.noreply.github.com>
2018-08-29 14:27:50 -0700

    Ex5 and study drill

---
+++ theHardWay/ex5_pt2.py
@@ -0,0 +1,14 @@
+#Conversion Calculator
+
+pounds = 12
+inches = 6
+
+#Inches to Centimeters
+# Inches * 2.54 = # Centimeteres
+# centimeters = inches * 2.54
+print(f"{inches} inches is", inches * 2.54, "centimeters")
+
+#Lbs to Kilograms
+# 1lb = 0.45359237 kg
+# lbs * 0.45359237 = kgs
+print(f"{pounds}lbs is", pounds * 0.45359237,"kilograms")
