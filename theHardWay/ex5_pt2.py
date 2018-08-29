845bbd92a33574350ba5322e251e3e574c1f761a
James Cor <jaeco@users.noreply.github.com>
2018-08-29 14:28:59 -0700

    Adding round function

--- theHardWay/ex5_pt2.py
+++ theHardWay/ex5_pt2.py
@@ -11,4 +11,4 @@ print(f"{inches} inches is", inches * 2.54, "centimeters")
 #Lbs to Kilograms
 # 1lb = 0.45359237 kg
 # lbs * 0.45359237 = kgs
-print(f"{pounds}lbs is", pounds * 0.45359237,"kilograms")
+print(f"{pounds} lbs is", round(pounds * 0.45359237),"kilograms")
