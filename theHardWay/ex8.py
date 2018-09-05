e476da1fc23f69bb97be06206bd1044c95062dba
James Cor <jaeco@users.noreply.github.com>
2018-09-05 13:39:00 -0700

    More comments for ex8

--- theHardWay/ex8.py
+++ theHardWay/ex8.py
@@ -2,11 +2,12 @@
 #  ITS LIKE AN ARRAY OF SORTS!...sort of?
 formatter = "{} {} {} {}"
 
-# print out a line with the variable formatter, using the format argument to pass in 4 numbers per argument
+# print out a line with the variable formatter, using the format argument to pass in 4 arguments of various types
 print(formatter.format(1, 2, 3, 4, 5)) #Extra arguments get ignored
 print(formatter.format("one", "two", "three", "four"))
-print(formatter.format(True, False, False, True))
-print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(True, False, False, True)) #these are keywords, Python recognizes that
+print(formatter.format(formatter, formatter, formatter, formatter)) #Why not just pass in the string itself?
+# White space? What white space?
 print(formatter.format(
     "Try your",
     "Own text here",
