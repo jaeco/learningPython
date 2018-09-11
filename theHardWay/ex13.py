ff0e0a3a936582543d971c00e89fa25a97b5372a
James Cor <jaeco@users.noreply.github.com>
2018-09-11 13:57:08 -0700

    ex13

---
+++ theHardWay/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("The first is called:", first)
+print("The second is called:", second)
+print("The third is called:", third)
